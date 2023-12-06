a3bae9c99764fe5251f3087b052c1b9c9be4dbe2
torkjel <torkjel@hongve.org>
2023-12-06 22:28:34 +0100

    Day 4 part 2

--- Day04.fs
+++ Day04.fs
@@ -17,7 +17,7 @@ let parseNumbers (str: string) =
         |> List.filter (fun f -> not ("".Equals f))
         |> List.map System.Convert.ToInt32
 
-let r =
+let processList (lines: List<string>) =
     lines
         |> List.map (fun a -> List.last (split ':' a))
         |> List.map (splitTuple2 '|')
@@ -33,4 +33,31 @@ let r =
         |> List.map (fun a -> pown 2 (a - 1))
         |> List.sum
 
-printfn $"{r}"
+let r = processList lines
+
+//printfn $"{r}"
+
+let parseScratchCards lines =
+    lines
+    |> List.map (fun a -> List.last (split ':' a))
+    |> List.map (splitTuple2 '|')
+    |> List.map (fun l ->
+        match l with
+        | (a ,b) -> (parseNumbers a, parseNumbers b)
+    )
+        |> List.map (fun l ->
+            match l with
+            | (a ,b) -> (Set.toList >> List.length) (Set.intersect (Set.ofList a) (Set.ofList b))
+        )
+
+let rec processWins (cards: List<int>) (depth: int)=
+    match depth with
+    | 0 -> 0
+    | _ ->
+        cards.Head + processWins cards.Tail (depth - 1) + processWins cards.Tail cards.Head 
+ 
+let cards = parseScratchCards lines
+
+let wins = processWins cards (List.length cards) + List.length cards
+
+printfn "%A" wins
